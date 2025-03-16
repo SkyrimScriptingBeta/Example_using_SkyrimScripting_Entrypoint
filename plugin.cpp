a732d6635fac88eabde0d7997779abd5101ef6bc
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-16 11:58:18 -0700

    Simpler :)

--- plugin.cpp
+++ plugin.cpp
@@ -1,10 +1,6 @@
 #include <RE/Skyrim.h>
 #include <SkyrimScripting/Entrypoint.h>
 
-SKSEPlugin_Init {
-    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* msg) {
-        if (msg->type == SKSE::MessagingInterface::kDataLoaded)
-            if (auto* consoleLog = RE::ConsoleLog::GetSingleton())
-                consoleLog->Print("Hello from Example using SkyrimScripting.Entrypoint!");
-    });
+SKSEPlugin_Entrypoint {
+    // ...
 }
